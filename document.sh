93321de732f7b1b66b3cb80eb685f48768efae97
erikgerrits <gerrits.erik@gmail.com>
2017-02-15 23:33:49 +0300

    Documentation update

--- document.sh
+++ document.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd src/lib
 typedoc --out ../../../mldoc --module commonjs --target ES2017 .
-cd ../mldoc
+cd ../../mldoc
 git add --all
 git commit -a -m "Documentation update"
 git push origin master
